06aeb416b23bd5953a8129c463368a06cf1f1635
kingorianderson <kingorianderson@gmail.com>
2022-03-01 16:39:09 +0300

    commit

--- models/Product.js
+++ models/Product.js
@@ -1,1 +1,39 @@
 import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        maxlength: 60,
+    },
+
+    desc: {
+        type: String,
+        required: true,
+        maxlength: 200,
+    },
+
+    img: {
+        type: String,
+        required: true,
+        maxlength: 60,
+    },
+
+    prices: {
+        type: [Number],
+        required: true
+    },
+
+    extraOptions: {
+        type: [
+            {
+                text: {type: String, required: true}, 
+                price: {type: Number, required: true}
+            },
+        ],
+    }
+  },
+    { timestamps: true }
+); 
+
+export default mongoose.models.Product || mongoose.model("Product", ProductSchema);
