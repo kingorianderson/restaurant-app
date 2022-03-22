9d9da8c8bf55e751c063b5d8e1b31a22aa4c0d4b
kingorianderson <kingorianderson@gmail.com>
2022-03-22 08:37:09 +0300

    commit

---
+++ pages/api/products/index.js
@@ -0,0 +1,20 @@
+import dbConnect from '../../../utility/mongo'
+import Product from '../../../models/Product'
+
+export default async function handler(req, res) {
+    const { method } = req;
+
+    dbConnect()
+
+    if(method === "GET"){}
+
+    if(method === "POST"){
+        try{
+            const product = await Product.create(req.body);
+            res.status(201).json(product);
+
+        }   catch(err){
+            res.status(500).json(err);
+        }
+    }
+  } 
